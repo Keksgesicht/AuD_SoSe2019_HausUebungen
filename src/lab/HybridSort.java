85cbd2b6fb4893118cda1e60d076a50617a03b2f
Jan B. <keksgesicht23@gmail.com>
2019-05-03 18:15:31 +0200

    H1b - Hilfsmethoden erstellt
    - HybridSort#partition(SortArray, int, int) für QuickSort
    - HybridSort#insertionSort(SortArray, int, int) für InsertionSort

--- src/lab/HybridSort.java
+++ src/lab/HybridSort.java
@@ -2,7 +2,10 @@ package lab;
 
 /**
  * Aufgabe H1b)
- * 
+ * @author Emre Berber
+ * @author Christoph Berst
+ * @author Jan Braun
+ *
  * Abgabe von: <name>, <name> und <name>
  */
 
@@ -19,7 +22,58 @@ public class HybridSort {
 	 */
 	public void sort(SortArray array, int k) {
 		assert(k>=0);
-		// TODO: Implement
+		
+		// Sorting
+		
+		return;
+	}
+	
+	/**
+	 * wendet den Partition-Algorithmus von QuickSort auf das Array an
+	 * @param array zu sortierendes Array
+	 * @param p untere Schranke
+	 * @param r obere Schranke
+	 * @return Postion der Problemzerlegung
+	 */
+	private int partition(SortArray array, int p, int r) {
+		Card x = array.getElementAt(p);
+		int i = p-1;
+		for(int j=0; j < r-1; j++) {
+			if(array.getElementAt(j).compareTo(x) <= 0) {
+				i++;
+				swap(array, i, j);
+			}
+		} swap(array, ++i, p);
+		return i;
+	}
+	
+	/**
+	 * Vertauscht zwei Elemente im Array
+	 * @param array zu sortierendes Array
+	 * @param i Position des ersten Element
+	 * @param j Position des zweiten Elements
+	 */
+	private void swap(SortArray array, int i, int j) {
+		Card cache = array.getElementAt(i);				// Zwischenspeicher für Element i erzeugen
+		array.setElementAt(i, array.getElementAt(j));	// i mit j überschreiben
+		array.setElementAt(j, cache);					// j auf den Wert des Zwischenspeichers setzen
+	}
+	
+	/**
+	 * sortiert Array mit Insertion-Sort zwischen der unteren und oberen Schranke
+	 * @param array zu sortierendes Array
+	 * @param p untere Schranke
+	 * @param r obere Schranke
+	 */
+	private void insertionSort(SortArray array, int p, int r) {
+		for(int j=p+1; j < r-1; j++) {
+			Card key = array.getElementAt(j);
+			int i = j-1;
+			while(i >= p && array.getElementAt(i).compareTo(key) > 0) {
+				array.setElementAt(i+1, array.getElementAt(i));
+				i--;
+			} array.setElementAt(i+1, key);
+		}
 	}
 	
 }
