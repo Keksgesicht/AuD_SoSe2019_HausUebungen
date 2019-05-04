0fa3c0821d956ebfdc5129765cba2684bd9b057d
Yusuf Emre Berber <Yusuf Emre Berber@192.168.11.11>
2019-05-04 11:16:51 +0200

    Jetzt funktioniert es

--- src/lab/HybridSortRandomPivot.java
+++ src/lab/HybridSortRandomPivot.java
@@ -15,15 +15,16 @@ import frame.SortArray;
 public class HybridSortRandomPivot extends HybridSort {
 	
 	protected int partition(SortArray array, int left, int right) {
-		int p = (int)(Math.random() * right-left) + left; 	 
+		int p = (int)(Math.random() * (right-left+1)) + left;	 
 		Card pivot = array.getElementAt(p); // Pivot-Element ist ein zuf‰lliges Element zwischen left und right
+		swap(array,p,left);
 		int i = right;
 		for(int j = right; j > left; j--) {
 			if(array.getElementAt(j).compareTo(pivot) > 0) {	// > oder >= ??
 				swap(array, i, j);
 				i--;
 			}
-		} swap(array, i, p); // Pivot an die richtige Stelle bringen		
+		} swap(array, i, left); // Pivot an die richtige Stelle bringen
 		return i; // neue Position zur√ºckgeben
 	}
 	
