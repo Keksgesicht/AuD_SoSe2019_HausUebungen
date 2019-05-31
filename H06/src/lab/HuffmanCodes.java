2d6f27ce0f2b661bb3088767f3ecded00fe28043
Jan B. <keksgesicht23@gmail.com>
2019-05-31 23:16:30 +0200

    H06.1 abc (not tested!)

--- H06/src/lab/HuffmanCodes.java
+++ H06/src/lab/HuffmanCodes.java
@@ -3,7 +3,9 @@ package lab;
 /**
  * Aufgabe H1
  * 
- * Abgabe von: <name>, <name> und <name>
+ * @author Emre Berber 2957148
+ * @author Christoph Berst 2743394
+ * @author Jan Braun 2768531
  */
 
 import java.io.ByteArrayInputStream;
@@ -39,21 +41,60 @@ public class HuffmanCodes {
 	 * Write the frequency of every byte in data into frequencyTable.
 	 */
 	public void buildFrequencyTable(byte[] data) {
-		// TODO
+		frequencyTable = new double[256];
+		
+		for(byte b : data) {
+			frequencyTable[b + 128]++;
+		}
+		for(double d : frequencyTable) {
+			d /= data.length;
+		}
 	}
 	
 	/**
 	 * Build a tree that defines the huffman code.
 	 */
 	public void buildHuffmanTree() {
-		// TODO
+		PriorityQueue<TreeNode> prioBytes = new PriorityQueue<TreeNode>();
+		for(short i=0; i < frequencyTable.length; i++) {
+			TreeNode tn = new TreeNode();
+			tn.frequency = frequencyTable[i];
+			tn.value = (byte) (i - 128);
+			prioBytes.add(tn);
+		}
+		while(prioBytes.size() > 1) {
+			TreeNode first = prioBytes.remove();
+			TreeNode second = prioBytes.remove();
+			
+			TreeNode newParent = new TreeNode(first, second, null, first.frequency + second.frequency, (byte) 0);
+			prioBytes.add(newParent);
+		}
+		huffmanTreeRoot = prioBytes.remove();
 	}
 	
 	/**
 	 * Build a table that gives the huffman code for each byte. Write that table to codeTable.
 	 */
 	public void buildHuffmanTable() {
-		// TODO
+		codeTable = new ArrayList[256];
+		buildHuffmanTable(huffmanTreeRoot, new ArrayList<Integer>());
+	}
+	
+	private void buildHuffmanTable(TreeNode tn, ArrayList<Integer> path) {
+		if(tn.left == null && tn.right == null)
+			codeTable[tn.value + 128] = (ArrayList<Integer>) path.clone();
+		else {
+			if(tn.left != null) {
+				path.add(0);
+				buildHuffmanTable(tn.left, path);
+				path.remove(path.size() -1);
+			}
+			if(tn.right != null) {
+				path.add(1);
+				buildHuffmanTable(tn.right, path);
+				path.remove(path.size() -1);
+			}
+		}
 	}
 	
 	/**
