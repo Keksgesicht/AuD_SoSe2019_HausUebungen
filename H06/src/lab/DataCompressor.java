37e6bda755ba07862adb9a45a7702a2dcb5961b6
Christoph <christophberst@yahoo.de>
2019-06-06 21:30:57 +0200

    H06 f) mit Fehlern

--- H06/src/lab/DataCompressor.java
+++ H06/src/lab/DataCompressor.java
@@ -21,13 +21,46 @@ public class DataCompressor {
 	 */
 	public void compress(byte[] inputData, ByteArrayOutputStream outputStream) {
 		HuffmanCodes huffman = new HuffmanCodes();
-		// TODO
+		huffman.buildFrequencyTable(inputData);
+		huffman.buildHuffmanTree();
+		huffman.buildHuffmanTable();
+
+		outputStream.write('A');
+		outputStream.write('U');
+		outputStream.write('D');
+		
+		BitOutputStream saver = new BitOutputStream(outputStream);
+		huffman.saveHuffmanTree(saver);
+		saver.close();
+		
+		outputStream.write(inputData.length);
+		
+		ByteArrayInputStream input = new ByteArrayInputStream(inputData);
+		
+		BitOutputStream output = new BitOutputStream(outputStream);
+		huffman.compress(input, output);
+		output.close();
 	}
 	/**
 	 * Decompress the inputStream and write the result to the outputStream.
 	 */
 	public void decompress(ByteArrayInputStream inputStream, ByteArrayOutputStream outputStream) {
 		HuffmanCodes huffman = new HuffmanCodes();
-		// TODO
+		char a = (char) inputStream.read();
+		char u = (char) inputStream.read();
+		char d = (char) inputStream.read();
+		if(a == 'A' && u == 'U' && d == 'D') {
+			BitInputStream loader = new BitInputStream(inputStream);
+			huffman.loadHuffmanTree(loader);
+			loader.close();
+			
+			int bytesToRead = (int) inputStream.read();
+			if(bytesToRead == -1) throw new EncodingException("inputStream has suddenly ended!");
+			
+			BitInputStream compressedData = new BitInputStream(inputStream);
+			huffman.decompress(compressedData, outputStream, bytesToRead);
+			compressedData.close();
+		}
+		else throw new EncodingException("wrong format!");
 	}
 }
