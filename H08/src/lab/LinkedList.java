559c0c2015f4004efea94fa103908413833b56ad
Jan B. <keksgesicht23@gmail.com>
2019-06-13 17:02:40 +0200

    H08 a) our own LinkedList!

--- H08/src/lab/LinkedList.java
+++ H08/src/lab/LinkedList.java
@@ -3,7 +3,9 @@ package lab;
 /**
  * Aufgabe H1 a)
  * 
- * Abgabe von: <name>, <name> und <name>
+ * @author Emre Berber 2957148
+ * @author Christoph Berst 2743394
+ * @author Jan Braun 2768531
  */
 
 import frame.ListNode;
@@ -33,28 +35,48 @@ public class LinkedList {
 	 * Return the number of elements in the linked list.
 	 */
 	public int length() {
-		// TODO
-		return -1;
+		int count = 0;
+		ListNode x = _head;
+		while(x != _nil) {
+			count++;
+			x = x.next();
+		}
+		return count;
 	}
 	
 	/**
 	 * Insert an entry into the linked list at the position before the given node.
 	 */
 	public void insertBefore(TableEntry entry, ListNode node) {
-		// TODO
+		ListNode newNode = new ListNode(entry, node.prev(), node);
+		node.prev().setNext(newNode);
+		node.setPrev(newNode);
+		if(node == head())
+			_head = newNode;		
 	}
 	
 	/**
 	 * Append an entry to the end of the list.
 	 */
 	public void append(TableEntry entry) {
-		// TODO
+		if(head() == nil()) {
+			_head = new ListNode(entry, _nil, _nil);
+			return;
+		}
+		ListNode iter = head();
+		while(iter.next() != _nil) {
+			iter = iter.next();
+		}
+		iter.setNext(new ListNode(entry, iter, _nil));
 	}
 	
 	/**
 	 * Delete the given node from the linked list.
 	 */
 	public void delete(ListNode node) {
-		// TODO
+		node.prev().setNext(node.next());
+		node.next().setPrev(node.prev());
+		if(node == _head)
+			_head = node.next();
 	}
 }
